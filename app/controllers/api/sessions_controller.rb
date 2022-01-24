b18c05ab2b686efb2a148fcf9ef663df1b8b9fc2
Charlie Sheppard <sheppard.charlie24@gmail.com>
2022-01-24 07:43:20 -0800

    Backend auth fully functional

--- app/controllers/api/sessions_controller.rb
+++ app/controllers/api/sessions_controller.rb
@@ -17,7 +17,7 @@ class Api::SessionsController < ApplicationController
       logout
       render "api/users/show"
     else
-      render json: ["Nobody signed in"], 404
+      render json: ["Nobody signed in"], status: 404
     end
   end
 
--- app/models/user.rb
+++ app/models/user.rb
@@ -1,7 +1,7 @@
 class User < ApplicationRecord
-  validates :email, :first_name, :last_name, :zip_code :password_digest, :session_token, presence: true
+  validates :first_name, :last_name, :email, :zip_code, :password_digest, :session_token, presence: true
   validates :email, uniqueness: true
-  validates :password, length: { maximum: 6 }, allow_nil: true
+  validates :password, length: { minimum: 6 }, allow_nil: true
 
   attr_reader :password
 
