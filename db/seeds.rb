70015a6c3a49c04aefdb2f27694ad61e110acb7a
Charlie Sheppard <sheppard.charlie24@gmail.com>
2022-01-26 17:42:44 -0800

    Add delete_all to seeds

--- db/seeds.rb
+++ db/seeds.rb
@@ -6,6 +6,7 @@
 #   movies = Movie.create([{ name: 'Star Wars' }, { name: 'Lord of the Rings' }])
 #   Character.create(name: 'Luke', movie: movies.first)
 User.delete_all
+Spot.delete_all
 
 demoUser = User.create({ first_name: 'Demo', last_name: 'User', email: 'demo@demo.com', zip_code: '05452', password: '123456' })
 
