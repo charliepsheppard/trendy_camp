64d2ec503c0e74c95d8a2a61ae809524ed6d7eb1
Charlie Sheppard <sheppard.charlie24@gmail.com>
2022-01-28 17:07:23 -0800

    Add background banner and style before search bar

---
+++ frontend/components/search/search_background.jsx
@@ -0,0 +1,11 @@
+import React from 'react';
+
+const SearchBackground = () => {
+  return (
+    <div className="search-background-container">
+      <img src={window.banner} className="search-background-img" alt=""/>
+    </div>
+  );
+};
+
+export default SearchBackground;
--- frontend/components/spots/spots_index.jsx
+++ frontend/components/spots/spots_index.jsx
@@ -1,5 +1,7 @@
 import React from 'react';
 import SpotIndexItem from './spot_index_item';
+import SearchBackground from '../search/search_background';
+// import banner from '../../../app/assets/images/banner-image.jpg'
 
 class SpotsIndex extends React.Component {
 
@@ -8,9 +10,13 @@ class SpotsIndex extends React.Component {
   }
 
   render() {
+    // const banner = require('../../../app/assets/images')
     return (
-      <div className="spot-index-item-container">
-        {this.props.spots.map(spot => <SpotIndexItem key={spot.id} spot={spot} />)}
+      <div>
+        <SearchBackground />
+        <div className="spot-index-item-container">
+          {this.props.spots.map(spot => <SpotIndexItem key={spot.id} spot={spot} />)}
+        </div>`
       </div>
     )
   };
