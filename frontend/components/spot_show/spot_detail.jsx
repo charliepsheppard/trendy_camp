867391bf60f97a803cd3a1614ace817d99a26b2b
Charlie Sheppard <sheppard.charlie24@gmail.com>
2022-02-02 21:44:27 -0800

    Add style to spot_detail

--- frontend/components/spot_show/spot_detail.jsx
+++ frontend/components/spot_show/spot_detail.jsx
@@ -24,6 +24,32 @@ class SpotDetail extends React.Component {
               <ReviewIndex reviews={reviews} session={this.props.session} deleteReview={this.props.deleteReview} />
               <button className="review-button"><Link to={`/spots/${spot.id}/reviews`} className="review-button-link">Leave a review</Link></button>
             </div>
+            <div>
+              <div></div>
+              <div></div>
+              <div></div>
+            </div>
+            <div className="checkin-details-container">
+              <div className="details-label">
+                <p>Details</p>
+              </div>
+              <div className="checkin-general">
+                <div className="checkin-details">
+                  <span className="checkin-detail"><span className="bold-checkin-detail">Check in: </span>After 2pm</span>
+                  <span className="checkin-detail"><span className="bold-checkin-detail">Check out: </span>Before 2pm</span>
+                  <span className="checkin-detail"><span className="bold-checkin-detail">Cancellation policy: </span>Strict</span>
+                </div>
+                <div className="checkin-details">
+                  <span className="checkin-detail"><span className="bold-checkin-detail">On arrival: </span>Meet and greet</span>
+                  <span className="checkin-detail"><span className="bold-checkin-detail">Minimum nights: </span>1 night</span>
+                  <span className="checkin-detail"><span className="bold-checkin-detail">Accepts bookings: </span>12 months out</span>
+                </div>
+                <div className="checkin-details">
+                  <span className="checkin-detail"><span className="bold-checkin-detail">Response time: </span>Within an hour</span>
+                  <span className="checkin-detail"><span className="bold-checkin-detail">Response rate: </span>87%</span>
+                </div>
+              </div>
+            </div>
           </div>
           <div className="booking-box">
             <h3 className="booking-box-info">${spot.price}</h3>
