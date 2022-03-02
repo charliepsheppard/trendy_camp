6751b87bd6124b3f5096ae495b76526e4176ea84
Charlie Sheppard <sheppard.charlie24@gmail.com>
2022-03-02 12:39:48 -0800

    Add error handling for email

--- app/models/user.rb
+++ app/models/user.rb
@@ -1,6 +1,7 @@
 class User < ApplicationRecord
   validates :first_name, :last_name, :email, :zip_code, :password_digest, :session_token, presence: true
   validates :email, uniqueness: true
+  validates :email, format: { with: URI::MailTo::EMAIL_REGEXP }
   validates :password, length: { minimum: 6 }, allow_nil: true
 
   attr_reader :password
