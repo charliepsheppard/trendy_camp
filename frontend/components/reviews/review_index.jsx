8af4b3f9c0e91321224a0bb9e17c356ed0e7dac1
Charlie Sheppard <sheppard.charlie24@gmail.com>
2022-03-03 10:29:16 -0800

    Add functionality to hide review and edit buttons

--- frontend/components/reviews/review_index.jsx
+++ frontend/components/reviews/review_index.jsx
@@ -2,9 +2,12 @@ import React from 'react';
 import ReviewIndexItem from './review_index_item';
 
 class ReviewIndex extends React.Component {
+  constructor(props) {
+    super(props)
+  }
 
   render() {
-    const reviews = this.props.reviews.map(review => <ReviewIndexItem key={review.id} review={review} deleteReview={this.props.deleteReview} />)
+    const reviews = this.props.reviews.map(review => <ReviewIndexItem key={review.id} session={this.props.session} review={review} deleteReview={this.props.deleteReview} />)
     return (
       <div>
         {reviews}
--- frontend/components/reviews/review_index_item.jsx
+++ frontend/components/reviews/review_index_item.jsx
@@ -6,6 +6,7 @@ class ReviewIndexItem extends React.Component {
     super(props);
 
     this.handleDelete = this.handleDelete.bind(this);
+    this.showEditAndDeleteButtons = this.showEditAndDeleteButtons.bind(this);
   }
 
   handleDelete(e) {
@@ -13,8 +14,21 @@ class ReviewIndexItem extends React.Component {
     this.props.deleteReview(this.props.review.id);
   }
 
+  showEditAndDeleteButtons() {
+    if (this.props.session === this.props.review.userId) {
+      return (
+        <div className="edit-delete-buttons">
+          <Link to={`/spots/${this.props.review.spotId}/reviews/${this.props.review.id}`} className="edit-button-link" ><button className="review-button">Edit review</button></Link>
+          <button className="review-button" onClick={this.handleDelete}>Delete review</button>
+        </div>
+      )
+    } else {
+      return <div className="edit-delete-buttons"></div>
+    }
+  }
+
   render() {
-    console.log('recommend props from review index item', this.props.review.recommended)
+    console.log('recommend props from review index item', this.props.session)
     return (
       <div className="review-content-container">
         <h3 className="review-content-heading">{this.props.review.title}</h3>
@@ -24,10 +38,11 @@ class ReviewIndexItem extends React.Component {
             </strong>. {this.props.review.recommended ? 'recommends this listing.' : 'does not recommend this listing.' }
         </p>
         <p className="review-body">{this.props.review.body}</p>
-        <div className="edit-delete-buttons">
+        {/* <div className="edit-delete-buttons">
           <Link to={`/spots/${this.props.review.spotId}/reviews/${this.props.review.id}`} className="edit-button-link" ><button className="review-button">Edit review</button></Link>
           <button className="review-button" onClick={this.handleDelete}>Delete review</button>
-        </div>
+        </div> */}
+        {this.showEditAndDeleteButtons()}
       </div>
     )
   };
