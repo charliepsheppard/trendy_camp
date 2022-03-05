14226b5812545286629b902619f68b4473663d8f
Charlie Sheppard <sheppard.charlie24@gmail.com>
2022-03-04 17:03:14 -0800

    Actual final commit for greenlight

--- frontend/components/footer/footer.jsx
+++ frontend/components/footer/footer.jsx
@@ -5,7 +5,6 @@ import { FaLinkedin } from "react-icons/fa";
 const Footer = () => {
   return (
       <div className="footer-container">
-        <p>© Charlie Sheppard | 2022</p>
         <div className="footer-social-links">
           <a href="https://www.linkedin.com/in/charlespsheppard/" target="_blank" className="footer-social-link"><FaLinkedin size={28} /></a>
           <a href="https://github.com/charliepsheppard/trendy_camp" target="_blank" className="footer-social-link"><FaGithub size={28} /></a>
