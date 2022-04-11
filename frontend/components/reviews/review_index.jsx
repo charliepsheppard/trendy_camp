489f46d975f94cd87f1cee48fce1c300356bcb35
Charlie Sheppard <sheppard.charlie24@gmail.com>
2022-04-11 16:25:02 -0700

    Change ReviewIndex to funtional component

--- frontend/components/reviews/review_index.jsx
+++ frontend/components/reviews/review_index.jsx
@@ -1,19 +1,20 @@
 import React from 'react';
 import ReviewIndexItem from './review_index_item';
 
-class ReviewIndex extends React.Component {
-  constructor(props) {
-    super(props)
-  }
-
-  render() {
-    const reviews = this.props.reviews.map(review => <ReviewIndexItem key={review.id} session={this.props.session} review={review} deleteReview={this.props.deleteReview} />)
-    return (
-      <div>
-        {reviews}
-      </div>
-    );
-  };
+const ReviewIndex = (props) => {
+  const reviews = props.reviews.map(review => {
+    <ReviewIndexItem
+      key={review.id}
+      session={props.session}
+      review={review}
+      deleteReview={props.deleteReview} 
+    />
+  })
+  return (
+    <div>
+      {reviews}
+    </div>
+  )
 }
 
 export default ReviewIndex;
